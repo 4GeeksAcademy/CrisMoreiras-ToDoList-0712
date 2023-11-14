f464cc1a537b22dcc38cceec2a11cbed60669da8
CrisMoreiras <137789637+CrisMoreiras@users.noreply.github.com>
2023-11-14 21:23:10 +0000

    03

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -8,8 +8,8 @@ const ToDo = () => {
 
 	return (
 		<div className="container">
+            <h1>My To Do List</h1>
 			<ul>
-                <h1>My To Do List</h1>
                 <li>
                     <input type="text" placeholder="Remember to do..."
                     onChange={(e)=>setInputToDo(e.target.value)}
@@ -25,19 +25,23 @@ const ToDo = () => {
                 {listToDo.map((item, index) => (
 
                     <li>
+                        <div className="form-check form-check-inline float-start">
+                            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                            <label className="form-check-label" for="flexCheckDefault"></label>
+                        </div>
                         
                         {item}{""}
 
-                        <a onClick={() => setListToDo(listToDo.filter((t,currentIndex) => index != currentIndex))
-                        }>
+                            <button type="button" className="border border-0 float-end"
+                            onClick={() => setListToDo(listToDo.filter((t,currentIndex) => index != currentIndex))
+                            }>
                             🗑️
-                        </a>                
-                            
+                            </button>                 
+                        
                     </li>
-
                 ))}       
             </ul>
-            <div>{listToDo.length} tasks.</div>
+            <p>{listToDo.length} tasks.</p>
 		</div>
 	);
 };
