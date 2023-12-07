514446878974179ed5b69b2069df54fdced81b8d
CrisMoreiras <137789637+CrisMoreiras@users.noreply.github.com>
2023-12-07 22:11:58 +0000

    02

--- src/js/component/test.jsx
+++ src/js/component/test.jsx
@@ -3,10 +3,26 @@ import React from "react";
 
 //create your first component
 const Test = () => {
+    
+    
 
     function traertareas (){
-        console.log (traertareas)
+        //console.log (traertareas)
         fetch("https://playground.4geeks.com/apis/fake/todos/user/criscasanovas")
+        .then((response)=>response.json())
+        .then((data)=>console.log(data))
+    }
+
+    function creartarea (){
+        console.log (creartarea)
+
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify([])
+        };
+
+        fetch("https://playground.4geeks.com/apis/fake/todos/user/criscasanovas2",requestOptions)
         .then((response)=>response.json())
         .then((list)=>console.log(list))
     }
@@ -15,6 +31,7 @@ const Test = () => {
 		<div className="text-center">
 			<h1 className="text-center mt-5">TEST!</h1>
             <button onClick={traertareas}>Traer Tareas</button>
+            <button onClick={creartarea}>Crear Tarea</button>
 		</div>
 	);
 };
