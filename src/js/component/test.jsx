0cddbe5a0116f26b800e609b52089bdd84ee57f8
CrisMoreiras <137789637+CrisMoreiras@users.noreply.github.com>
2023-12-08 18:02:04 +0000

    05

--- src/js/component/test.jsx
+++ src/js/component/test.jsx
@@ -5,32 +5,19 @@ import React, {useState, useEffect} from "react";
 const Test = () => {
     
     const [list, setList] = useState ([])
-    const [elements, setElements] = useState([])
+   
 
     function getList(){
-        //console.log("getList")
+        console.log("getList")
         fetch("https://playground.4geeks.com/apis/fake/todos/user/criscasanovas")
         .then((response)=>response.json())
-        .then((data)=>{
-            setList(data);    
-        })
+        .then((data)=> setList(data))    
         }
         
-    
-
     useEffect (()=>{
-        console.log ("cargo componente")
-        getList();
+        //console.log ("cargo componente")
+        getList()
     },[])
-
-    
-
-    //function traertareas (){
-        //console.log (traertareas)
-        //fetch("https://playground.4geeks.com/apis/fake/todos/user/criscasanovas")
-        //.then((response)=>response.json())
-        //.then((data)=>console.log(data))
-    //}
 
     function creartarea (){
         console.log (creartarea)
@@ -47,15 +34,13 @@ const Test = () => {
     }
 
 	return (
-        <>
-        
+
 		<div className="text-center">
 			<h1 className="text-center mt-5">TEST!</h1>
-            {list.map((item)=> <p>item</p> )}
             <button onClick={getList}>Traer Tareas</button>
             <button onClick={creartarea}>Crear Tarea</button>
+            {list.map((item)=> <p>{item.label}</p>)}  
 		</div>
-        </>
 	);
 };
 
